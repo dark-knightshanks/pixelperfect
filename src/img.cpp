49ee5cab84993ff9fdd6a14b429aced937170af8
knight <rishirajrajgor@gmail.com>
2026-07-05 23:39:10 +0530

    update img.cpp

--- src/img.cpp
+++ src/img.cpp
@@ -5,22 +5,31 @@
 #include "ray.h"
 
 // check if ray hits the sphere with center , solve for t 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = center - r.origin();
     auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
+    auto h = dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+    auto discriminant = h*h - a*c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (h - std::sqrt(discriminant) ) / a;
+    }
 }
 
 // we use and y not x bcoz x is changing iteratively  inside y and hence we find a pattern of blend when changed to x which repeats itself
 color ray_color(const ray& r){
-    if (hit_sphere(point3(0,0,-1), 0.5, r))
-        return color(1, 0, 0);
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1); // interpolation used for blending: blendedValue=(1−a)⋅startValue+a⋅endValue,
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0); // interpolation used for blending: blendedValue=(1−a)⋅startValue+a⋅endValue,
 }
 
 int main(){
